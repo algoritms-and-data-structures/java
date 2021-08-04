0509f6ef4227ae30bfab404a9e152b37aea05b2b
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-08-04 20:14:05 +0200

    add data structure: queue implemented in arrayList

---
+++ src/data_structures/queue/QueueArrayList.java
@@ -0,0 +1,35 @@
+package data_structures.queue;
+
+import java.util.ArrayList;
+
+public class QueueArrayList implements Queue {
+    private ArrayList<E> queue = new ArrayList<E>();
+
+    @Override
+    public boolean isEmpty() { // Returns true if the queue contains no elements
+        if(queue.size() == 0) return true;
+
+        return false;
+    }
+
+    @Override
+    public E peek() { // Returns the element at the front of the queue.
+        if(isEmpty()) return null;
+
+        return queue.get(0);
+    }
+
+    @Override
+    public void enqueue(E element) { // Inserts the specified element at the rear of the queue
+        queue.add(element);
+    }
+
+    @Override
+    public E dequeue() { // Removes and returns the element at the front of the queue
+        if(isEmpty()) return null;
+
+        queue.remove(0);
+
+        return peek();
+    }
+}
