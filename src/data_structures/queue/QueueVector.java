ce3df1ef1d700f8efee52a0b666c996d07b8c6fe
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-08-04 20:17:07 +0200

    add data structure: queue implemented in vector

---
+++ src/data_structures/queue/QueueVector.java
@@ -0,0 +1,36 @@
+package data_structures.queue;
+
+import java.util.Vector;
+
+public class QueueVector implements Queue {
+    private Vector<E> queue = new Vector<E>();
+
+    @Override
+    public boolean isEmpty() { // Returns true if the queue contains no elements
+        if(queue.size() == 0) return true;
+
+        return false;
+    }
+
+    @Override
+    public E peek() { // Returns the element at the front of the queue.
+        if(isEmpty()) return null;
+
+        return queue.elementAt(0);
+    }
+
+    @Override
+    public void enqueue(E element) { // Inserts the specified element at the rear of the queue
+        queue.add(element);
+        queue.trimToSize();
+    }
+
+    @Override
+    public E dequeue() { // Removes and returns the element at the front of the queue
+        if(isEmpty()) return null;
+
+        queue.removeElementAt(0);
+
+        return peek();
+    }
+}
