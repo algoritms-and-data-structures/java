f7627f7c40ad092f7baa18e1772f2332e28ba27d
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-08-04 20:05:45 +0200

    add data structure: queue implemented in array

---
+++ src/data_structures/queue/Queue.java
@@ -0,0 +1,14 @@
+package data_structures.queue;
+
+public interface Queue {
+
+    public boolean isEmpty();
+
+//    public int size();
+
+    public E peek();
+
+    public void enqueue(E element);
+
+    public E dequeue();
+}
---
+++ src/data_structures/queue/QueueArray.java
@@ -0,0 +1,51 @@
+package data_structures.queue;
+
+import java.util.Arrays;
+
+public class QueueArray implements Queue {
+    private E[] queue = new E[10];
+    private int size = 0;
+
+    @Override
+    public boolean isEmpty() { // Returns true if the queue contains no elements
+        if (size == 0) return true;
+
+        return false;
+    }
+
+    @Override
+    public E peek() { // Returns the element at the front of the queue.
+        if(isEmpty()) return null;
+
+        return queue[0];
+    }
+
+    @Override
+    public void enqueue(E element) {  // Inserts the specified element at the rear of the queue
+        if(size == queue.length) increaseSize();
+
+        queue[size] = element;
+        size++;
+    }
+
+    @Override
+    public E dequeue() { // Removes and returns the element at the front of the queue
+        if(isEmpty()) return null;
+
+        queue[0] = null;
+
+        for(int i = 1; i < size; i++) {
+            queue[i-1] = queue[i];
+        }
+        
+        queue[size-1] = null;
+        size--;
+
+        return peek();
+    }
+
+    private void increaseSize() {
+        int newSize = queue.length * 2;
+        queue = Arrays.copyOf(queue, newSize);
+    }
+}
